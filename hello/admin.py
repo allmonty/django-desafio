0858a2d0478c5e01c336df73134ad72370d48581
Allan David <allanbrados@gmail.com>
2017-11-16 00:41:00 -0200

    Ready to start

--- hello/admin.py
+++ hello/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from hello.models import *
+from django import forms
 
 # Register your models here.
+
+class GreetingAdmin(admin.ModelAdmin):
+    list_display = ['when']
+    list_filter = ['when']
+
+admin.site.register(Greeting, GreetingAdmin)
