466a46ea709648e7b8c1ee5e8433d60e5a96546e
Allan David <allanbrados@gmail.com>
2017-11-17 01:41:28 -0200

    Creates Wallet model and links many cards to one wallet

--- api/admin.py
+++ api/admin.py
@@ -4,8 +4,14 @@ from django import forms
 
 # Register your models here.
 
+class WalletAdmin(admin.ModelAdmin):
+    list_display = ['owner_name']
+    list_filter = ['owner_name']
+
+admin.site.register(Wallet, WalletAdmin)
+
 class CreditCardAdmin(admin.ModelAdmin):
-    list_display = ['number', 'due_date', 'expiration_date', 'cvv', 'limit', 'avilable_amount']
-    list_filter = ['due_date', 'expiration_date', 'limit', 'avilable_amount']
+    list_display = ['number', 'wallet', 'due_date', 'expiration_date', 'cvv', 'limit', 'avilable_amount']
+    list_filter = ['due_date', 'expiration_date', 'limit', 'avilable_amount', 'wallet']
 
 admin.site.register(CreditCard, CreditCardAdmin)
--- api/models.py
+++ api/models.py
@@ -8,3 +8,10 @@ class CreditCard(models.Model):
     cvv             = models.CharField('CVV', max_length=255, default=None, null=True, blank=True)
     limit           = models.CharField('Limit', max_length=255, default=None, null=True, blank=True)
     avilable_amount = models.CharField('Available amount', max_length=255, default=None, null=True, blank=True)
+    wallet          = models.ForeignKey('Wallet', null=True, on_delete=models.CASCADE)
+
+class Wallet(models.Model):
+    owner_name = models.CharField('Owner name', max_length=255, default=None, null=True, blank=True)
+
+    def __str__(self):
+        return "%s's wallet" % (self.owner_name)
